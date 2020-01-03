05c96756deba126c80c008dc8c43d3514aefdffd
ascdso <ascdso2020@gmail.com>
2020-01-02 21:54:20 -0700

    Define terrafor/awsm required version commit

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
+terraform {
+  required_version = ">= 0.12, < 0.13"
+}
+
 provider"aws"{
   region="us-west-2"
+
+  # Allow any 2.x version of the AWS provider
+  version = "~> 2.0"
 }
 
 resource"aws_instance" "rhel8"{
