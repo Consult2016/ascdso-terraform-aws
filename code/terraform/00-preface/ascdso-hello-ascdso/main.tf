42ca4fcc8e9676e5a5a8de956bb3f1623c386f8e
ascdso <ascdso2020@gmail.com>
2020-03-04 15:12:27 -0700

    ascdso - committed busybox httpd to ascdso-terraform-master ami instance

--- code/terraform/00-preface/ascdso-hello-ascdso/main.tf
+++ code/terraform/00-preface/ascdso-hello-ascdso/main.tf
@@ -12,6 +12,13 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-087c2c50437d0b80d"
   instance_type = "t2.micro"
+  vpc_security_group_ids = "vpc-a655c5de"
+
+  user_data = <<-EOF
+            #!/bin/bash
+            echo "Hello, ASCDSO!!!" > index.html
+            nohup busybox httpd -f -p 8080 &
+            EOF
 
   tags = {
      Name = "ascdso-terraform-master"
